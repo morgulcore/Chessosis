d7532cd24999ed4ea058ce43ea12cae2c5b928f6
Henrik Lindberg <hlindberg@hotmail.fi>
2015-05-14 12:55:58 +0300

    Thu May 14 12:55:58 EEST 2015

---
+++ ChessosisNBProject/src/main/java/chessosisnbproject/chessosisnbproject/ChessosisNBProject.java
@@ -0,0 +1,11 @@
+package chessosisnbproject.chessosisnbproject;
+
+/**
+ *
+ * @author henrik
+ */
+public class ChessosisNBProject {
+    public static void main( String[] args ) {
+        System.out.println("Hello, world!");
+    }
+}
